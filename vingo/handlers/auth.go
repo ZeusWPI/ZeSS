f42f0f9640340f29912d52817bc4b0e6cfbc714e
Hannes <hannes-dev@mailbox.org>
2024-05-05 14:09:32 +0200

    vingo: why cant a session store structs :(

--- vingo/handlers/auth.go
+++ vingo/handlers/auth.go
@@ -112,8 +112,8 @@ func Callback(c *fiber.Ctx) error {
 	}
 
 	sess.Regenerate()
-	sess.Set(USER_ID, zauth_user.Id)
-	sess.Set(USERNAME, zauth_user.Username)
+	user := StoreUser{Id: zauth_user.Id, Username: zauth_user.Username}
+	sess.Set(STORE_USER, &user)
 	sess.Save()
 
 	return c.Status(200).Redirect("/")
--- vingo/handlers/card.go
+++ vingo/handlers/card.go
@@ -10,18 +10,16 @@ import (
 func StartCardRegister(c *fiber.Ctx) error {
 	// keep track of the user that initiated the request in global state
 	// since only one user can be registering a card at a time
-	sess, err := store.Get(c)
-	if err != nil {
-		log.Println(err)
-		return c.Status(500).SendString("Session error")
+	user := GetUserFromStore(c)
+	if user == nil {
+		return c.Status(401).Redirect("/login")
 	}
-	user_id := sess.Get(USER_ID).(int)
 
 	if time.Now().Before(registering_end) {
 		return c.Status(400).SendString("Another user is already registering a card")
 	}
 
-	registering_user = user_id
+	registering_user = user.Id
 	registering_end = time.Now().Add(time.Minute)
 
 	log.Println("Card registration started by user", registering_user)
