66eb2699a2dfe0ec9d0c13881e1295662b2fff01
Hannes <hannes-dev@mailbox.org>
2024-06-05 22:37:39 +0200

    vingo: add settings table

---
+++ vingo/database/settings.go
@@ -0,0 +1,35 @@
+package database
+
+type Settings struct {
+	ScanInOut   bool
+	Leaderboard bool
+	Public      bool
+}
+
+var (
+	settingsCreateStmt = `
+		CREATE TABLE IF NOT EXISTS settings (
+			user_id INT NOT NULL PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+			public BOOLEAN NOT NULL DEFAULT FALSE,
+			scan_in_out BOOLEAN NOT NULL DEFAULT FALSE,
+			leaderboard BOOLEAN NOT NULL DEFAULT TRUE
+		);
+	`
+)
+
+func CreateSettings(user_id int) error {
+	_, err := db.Exec("INSERT INTO settings (user_id) VALUES ($1) ON CONFLICT DO NOTHING;", user_id)
+	return err
+}
+
+func GetSettings(user_id int) (*Settings, error) {
+	row := db.QueryRow("SELECT scan_in_out FROM scans WHERE user_id = $1;", user_id)
+	settings := new(Settings)
+	err := row.Scan(&settings.ScanInOut)
+	return settings, err
+}
+
+func UpdateSettings(user_id int, settings Settings) error {
+	_, err := db.Exec("UPDATE settings SET scan_in_out = $1, leaderboard = $2, public = $3 WHERE user_id = $4;", settings.ScanInOut, settings.Leaderboard, settings.Public, user_id)
+	return err
+}
--- vingo/database/users.go
+++ vingo/database/users.go
@@ -1,11 +1,10 @@
 package database
 
 type User struct {
-	Id          int
-	Username    string
-	Admin       bool
-	Leaderboard bool
-	Public      bool
+	Id       int
+	Username string
+	Admin    bool
+	Settings Settings
 }
 
 var (
@@ -14,34 +13,43 @@ var (
 			id INTEGER PRIMARY KEY,
 			username TEXT NOT NULL,
 			admin BOOLEAN DEFAULT FALSE NOT NULL,
-			leaderboard BOOLEAN DEFAULT FALSE NOT NULL,
-			public BOOLEAN DEFAULT FALSE NOT NULL,
 			created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 		);
 	`
 )
 
 func CreateUserIfNew(user_id int, username string) error {
-	_, err := db.Exec("INSERT INTO users (id, username) VALUES ($1, $2) ON CONFLICT DO NOTHING;", user_id, username)
+	_, err := db.Exec("INSERT INTO users (id, username) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET username = EXCLUDED.username;", user_id, username)
+	if err != nil {
+		return err
+	}
+
+	err = CreateSettings(user_id)
 	return err
 }
 
 func GetUser(user_id int) (*User, error) {
-	return getUser("SELECT id, username, admin, leaderboard, public FROM users WHERE id = $1;", user_id)
+	return getUser("SELECT id, username, admin, scan_in_out, leaderboard, public FROM users JOIN settings on id = user_id WHERE id = $1;", user_id)
 }
 
 func GetUserFromCard(card_serial string) (*User, error) {
-	return getUser(`
+	row := db.QueryRow(`
 		SELECT users.id, users.username, users.admin, users.leaderboard, users.public
 		FROM users
 		JOIN cards ON users.id = cards.user_id
 		WHERE cards.serial = $1;
 	`, card_serial)
+	user := new(User)
+	err := row.Scan(&user.Id, &user.Username, &user.Admin)
+	return user, err
 }
 
 func getUser(query string, args ...interface{}) (*User, error) {
 	row := db.QueryRow(query, args...)
 	user := new(User)
-	err := row.Scan(&user.Id, &user.Username, &user.Admin, &user.Leaderboard, &user.Public)
+	settings := new(Settings)
+	err := row.Scan(&user.Id, &user.Username, &user.Admin, &settings.ScanInOut, &settings.Leaderboard, &settings.Public)
+
+	user.Settings = *settings
 	return user, err
 }
