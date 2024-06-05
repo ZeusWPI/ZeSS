d1a90359e63785dee66fe17d0d044f318f7e19e3
Hannes <hannes-dev@mailbox.org>
2024-06-05 23:18:24 +0200

    vingo: add settings

---
+++ vingo/handlers/settings.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"vingo/database"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func SettingsUpdate(c *fiber.Ctx) error {
+	user := getUserFromStore(c)
+
+	scan_in_out := c.FormValue("scan_in_out")
+	leaderboard := c.FormValue("leaderboard")
+	public := c.FormValue("public")
+
+	if scan_in_out == "" || leaderboard == "" || public == "" {
+		return c.Status(400).SendString("Missing fields")
+	}
+
+	settings := database.Settings{
+		ScanInOut:   scan_in_out == "on",
+		Leaderboard: leaderboard == "on",
+		Public:      public == "on",
+	}
+
+	sess, _ := store.Get(c)
+	database.UpdateSettings(user.Id, settings)
+	user, _ = database.GetUser(user.Id)
+	sess.Set(STORE_USER, &user)
+	sess.Save()
+
+	return c.Redirect("/settings")
+}
