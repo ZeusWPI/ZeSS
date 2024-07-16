b618181226c068bd4ae0e605435037b25df4c9ee
draxaris1010 <axel.dezittere@gmail.com>
2024-07-16 18:30:01 +0200

    vinscant: rust edition | a beginning wifi works http request works

--- vinscant/mpfiles/lib/term_color.py
+++
@@ -1,9 +0,0 @@
-# Ansi color codes
-RESET     = "\x1b[0m"
-BOLD      = "\x1b[1m"
-UNDERLINE = "\x1b[4m"
-BLACK     = "\x1b[30m"
-RED       = "\x1b[31m"
-GREEN     = "\x1b[32m"
-YELLOW    = "\x1b[33m"
-BLUE      = "\x1b[34m"
--- vinscant/upload_file.sh
+++
@@ -1,2 +0,0 @@
-# ./upload_file.sh <password> <ip> <file>
-python webrepl_cli.py -p $1 $3 $2:$3
