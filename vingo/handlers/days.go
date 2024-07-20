07b59498ebf9d81069ca385bda20128f4a69a865
Hannes <hannes-dev@mailbox.org>
2024-07-20 16:36:22 +0200

    vingo: make days endpoint use json

--- vingo/handlers/days.go
+++ vingo/handlers/days.go
@@ -7,19 +7,21 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func DaysRegister(c *fiber.Ctx) error {
-	form_start_date := c.FormValue("start_date")
-	form_end_date := c.FormValue("end_date")
+type Days struct{}
 
-	start_date, start_err := time.Parse("2006-01-02", form_start_date)
-	end_date, end_err := time.Parse("2006-01-02", form_end_date)
-	if start_err != nil || end_err != nil {
-		logger.Println("Error parsing dates:", start_err, end_err)
-		return c.Status(400).SendString("Error parsing dates")
+type DaysBody struct {
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+}
+
+func (Days) CreateMultiple(c *fiber.Ctx) error {
+	payload := new(DaysBody)
+	if err := c.BodyParser(payload); err != nil {
+		logger.Println("Error parsing body:", err)
+		return c.Status(400).SendString("Error parsing body")
 	}
 
-	logger.Println("Registering days from", start_date, "to", end_date)
-	err := database.CreateDays(start_date, end_date)
+	err := database.CreateDays(payload.StartDate, payload.EndDate)
 	if err != nil {
 		logger.Println("Error creating days:", err)
 		return c.Status(500).SendString("Error creating days")
@@ -28,9 +30,8 @@ func DaysRegister(c *fiber.Ctx) error {
 	return c.Redirect("/days")
 }
 
-func DaysDelete(c *fiber.Ctx) error {
+func (Days) Delete(c *fiber.Ctx) error {
 	day_id := c.Params("id")
-	logger.Println("Deleting day", day_id)
 	err := database.DeleteDay(day_id)
 	if err != nil {
 		logger.Println("Error deleting day:", err)
