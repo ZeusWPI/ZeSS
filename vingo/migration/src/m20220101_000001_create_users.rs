40718f16b2ff093a8adbb7c42849eb5bb918271f
Hannes Klinckaert <hannes-dev@mailbox.org>
2024-08-29 20:38:03 +0200

    vingo: add seaorm and user table

---
+++ vingo/migration/src/m20220101_000001_create_users.rs
@@ -0,0 +1,41 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    Admin,
+    CreatedAt,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .col(ColumnDef::new(User::Id).integer().primary_key())
+                    .col(text(User::Name))
+                    .col(boolean(User::Admin).default(false))
+                    .col(
+                        timestamp_with_time_zone(User::CreatedAt)
+                            .default(Expr::current_timestamp()),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
