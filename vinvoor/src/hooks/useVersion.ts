6311117308b7e8cb3c2893f784c944d4de3dc7ee
Topvennie <vincent@vallaeys.com>
2024-10-20 15:04:31 +0200

    vinvoor: footer

---
+++ vinvoor/src/hooks/useVersion.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { convertVersionJSON, Version, VersionJSON } from '../types/version';
+import { getApi } from "../util/fetch";
+
+const ENDPOINT = "version";
+
+export const useVersion = () =>
+  useQuery<Version>({
+    queryKey: ["version"],
+    queryFn: () => getApi<Version, VersionJSON>(ENDPOINT, convertVersionJSON),
+    retry: 1
+  })
