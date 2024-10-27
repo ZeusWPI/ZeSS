9a2ca86ef01a95fc962c40f8a1a74c78c6d39b07
Topvennie <vincent@vallaeys.com>
2024-10-27 17:47:55 +0100

    vinvoor: format

--- vinvoor/src/footer/Footer.tsx
+++ vinvoor/src/footer/Footer.tsx
@@ -7,44 +7,44 @@ export const Footer = () => {
   const { data: version } = useVersion();
 
   return (
-      <Box
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <TypographyG>v {version?.version ?? ""}</TypographyG>
+      <TypographyG
         sx={{
+          height: "100%",
           display: "flex",
-          justifyContent: "space-between",
           alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <TypographyG>v {version?.version ?? ""}</TypographyG>
-        <TypographyG
-          sx={{
-            height: "100%",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          Made with ❤️ by
-          <Link href="http://zeus.gent">
-            <Icon
-              sx={{
-                pl: "4px",
-                height: "40px",
-                display: "flex",
-                alignItems: "center",
-                width: "60px",
-              }}
-            >
-              <img
-                src={ZeusIcon}
-                alt="Zeus Icon"
-                style={{ height: "auto", width: "40px" }}
-              />
-            </Icon>
-          </Link>
-        </TypographyG>
-        <Link href="https://github.com/ZeusWPI/ZeSS" textAlign="end">
-          © 2024
+        Made with ❤️ by
+        <Link href="http://zeus.gent">
+          <Icon
+            sx={{
+              pl: "4px",
+              height: "40px",
+              display: "flex",
+              alignItems: "center",
+              width: "60px",
+            }}
+          >
+            <img
+              src={ZeusIcon}
+              alt="Zeus Icon"
+              style={{ height: "auto", width: "40px" }}
+            />
+          </Icon>
         </Link>
-      </Box>
+      </TypographyG>
+      <Link href="https://github.com/ZeusWPI/ZeSS" textAlign="end">
+        © 2024
+      </Link>
+    </Box>
   );
 };
