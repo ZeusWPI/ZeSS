d624a0616f9aab04f452b9b21caa2f977470a7bf
Topvennie <vincent@vallaeys.com>
2024-10-28 17:25:13 +0100

    vinvoor: navbar fullwidth

--- vinvoor/src/App.tsx
+++ vinvoor/src/App.tsx
@@ -25,7 +25,7 @@ export const App = () => {
         minHeight: "100vh",
       }}
     >
-      <Container sx={{ padding: 0 }}>
+      <Container maxWidth="xl" sx={{ padding: 0 }}>
         <NavBar />
       </Container>
       <Container
