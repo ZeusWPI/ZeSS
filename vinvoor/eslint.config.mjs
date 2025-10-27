6aa1f710e7d432c4e54cec2baee7b6b13b863ebf
Topvennie <vincent@vallaeys.com>
2025-10-27 13:44:16 +0100

    vinvoor: update eslint

--- vinvoor/eslint.config.mjs
+++ vinvoor/eslint.config.mjs
@@ -1,24 +1,42 @@
-import antfu from "@antfu/eslint-config";
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
 
-export default antfu(
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
-    stylistic: {
-      quotes: "double",
-      semi: true,
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, react.configs.flat.recommended, react.configs.flat['jsx-runtime']],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        }
+      }
     },
-
-    react: true,
-
-    typescript: {
-      tsconfigPath: "./tsconfig.json",
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
-
-    formatters: true,
-
     rules: {
-      "react-hooks/exhaustive-deps": "off",
-      "ts/switch-exhaustiveness-check": "off",
-      "ts/strict-boolean-expressions": "off",
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-namespace": "off",
+      "react/display-name": "off",
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      }
     },
   },
-);
+)
